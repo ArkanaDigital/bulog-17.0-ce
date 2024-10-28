e95aa90fa9c99b624f47ab3d44cbfa872166df30
muhammad irwan <m.iirwann@gmail.com>
2024-10-28 16:59:54 +0700

    [PULL] 24-10-2024

--- addons/account/models/account_account.py
+++ addons/account/models/account_account.py
@@ -101,7 +101,14 @@ class AccountAccount(models.Model):
     note = fields.Text('Internal Notes', tracking=True)
     company_id = fields.Many2one('res.company', string='Company', required=True, readonly=False,
         default=lambda self: self.env.company)
-    tag_ids = fields.Many2many('account.account.tag', 'account_account_account_tag', string='Tags', help="Optional tags you may want to assign for custom reporting", ondelete='restrict')
+    tag_ids = fields.Many2many(
+        comodel_name='account.account.tag',
+        relation='account_account_account_tag',
+        string='Tags',
+        help="Optional tags you may want to assign for custom reporting",
+        ondelete='restrict',
+        tracking=True,
+    )
     group_id = fields.Many2one('account.group', compute='_compute_account_group', store=True, readonly=True,
                                help="Account prefixes can determine account groups.")
     root_id = fields.Many2one('account.root', compute='_compute_account_root', store=True, precompute=True)
@@ -452,7 +459,11 @@ class AccountAccount(models.Model):
     @api.depends('account_type')
     def _compute_reconcile(self):
         for account in self:
-            account.reconcile = account.account_type in ('asset_receivable', 'liability_payable')
+            if account.internal_group in ('income', 'expense', 'equity'):
+                account.reconcile = False
+            elif account.account_type in ('asset_receivable', 'liability_payable'):
+                account.reconcile = True
+            # For other asset/liability accounts, don't do any change to account.reconcile.
 
     def _set_opening_debit(self):
         for record in self:
@@ -637,6 +648,8 @@ class AccountAccount(models.Model):
         for company, company_accounts in accounts_per_company.items():
             company._update_opening_move({account: data[account.id] for account in company_accounts})
 
+        self.env.flush_all()
+
     def _toggle_reconcile_to_true(self):
         '''Toggle the `reconcile´ boolean from False -> True
 
@@ -715,6 +728,12 @@ class AccountAccount(models.Model):
                     raise UserError(_('You cannot set a currency on this account as it already has some journal entries having a different foreign currency.'))
 
         return super(AccountAccount, self).write(vals)
+
+    def _load_records_write(self, values):
+        if 'prefix' in values:
+            del values['code_digits']
+            del values['prefix']
+        super()._load_records_write(values)
 
     @api.ondelete(at_uninstall=False)
     def _unlink_except_contains_journal_items(self):
@@ -894,7 +913,9 @@ class AccountGroup(models.Model):
             company_ids = account_ids.company_id.root_id.ids
             account_ids = account_ids.ids
         else:
-            company_ids = self.company_id.ids
+            company_ids = []
+            for company in self.company_id:
+                company_ids.extend(company._accessible_branches().ids)
             account_ids = []
         if not company_ids and not account_ids:
             return
@@ -951,13 +972,13 @@ class AccountGroup(models.Model):
                    AND parent.id != child.id
                    AND parent.company_id = child.company_id
                  WHERE child.company_id IN %s
-                   AND child.parent_id IS DISTINCT FROM parent.id -- IMPORTANT avoid to update if nothing changed
               ORDER BY child.id, char_length(parent.code_prefix_start) DESC
             )
             UPDATE account_group child
                SET parent_id = relation.parent_id
               FROM relation
              WHERE child.id = relation.child_id
+               AND child.parent_id IS DISTINCT FROM relation.parent_id
          RETURNING child.id
         """, tuple(company_ids))
         self.env.cr.execute(query)
--- addons/sale_timesheet/models/sale_order.py
+++ addons/sale_timesheet/models/sale_order.py
@@ -44,7 +44,11 @@ class SaleOrder(models.Model):
         timesheet_unit_amount_dict = defaultdict(float)
         timesheet_unit_amount_dict.update({order.id: unit_amount for order, unit_amount in group_data})
         for sale_order in self:
-            total_time = sale_order.company_id.project_time_mode_id._compute_quantity(timesheet_unit_amount_dict[sale_order.id], sale_order.timesheet_encode_uom_id)
+            total_time = sale_order.company_id.project_time_mode_id._compute_quantity(
+                timesheet_unit_amount_dict[sale_order.id],
+                sale_order.timesheet_encode_uom_id,
+                rounding_method='HALF-UP',
+            )
             sale_order.timesheet_total_duration = round(total_time)
 
     def _compute_field_value(self, field):
@@ -93,6 +97,7 @@ class SaleOrder(models.Model):
         precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
         return self.order_line.filtered(lambda sol:
             sol.is_service
+            and sol.invoice_status != "invoiced"
             and not sol.has_displayed_warning_upsell  # we don't want to display many times the warning each time we timesheet on the SOL
             and sol.product_id.service_policy == 'ordered_prepaid'
             and float_compare(
@@ -138,12 +143,19 @@ class SaleOrder(models.Model):
 
         return action
 
+    def _reset_has_displayed_warning_upsell_order_lines(self):
+        precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
+        for line in self.order_line:
+            if line.has_displayed_warning_upsell and line.product_uom and float_compare(line.qty_delivered, line.product_uom_qty, precision_digits=precision) == 0:
+                line.has_displayed_warning_upsell = False
+
     def _create_invoices(self, grouped=False, final=False, date=None):
         """Link timesheets to the created invoices. Date interval is injected in the
         context in sale_make_invoice_advance_inv wizard.
         """
         moves = super()._create_invoices(grouped=grouped, final=final, date=date)
         moves._link_timesheets_to_invoice(self.env.context.get("timesheet_start_date"), self.env.context.get("timesheet_end_date"))
+        self._reset_has_displayed_warning_upsell_order_lines()
         return moves
 
 
